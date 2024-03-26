d53a873df90b92d932fccfa50ddf397df013d13b
Wahid Contractor <wcontractor@gmail.com>
2024-03-26 19:48:07 -0400

    Update streamlit_app.py
    Added call to fruity vice API and display results as fv_df

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
-
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(" :cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -24,6 +24,9 @@ ingredients_list = st.multiselect(
     , my_dataframe
     , max_selections=5
 )
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 if ingredients_list:
     st.write(ingredients_list)
