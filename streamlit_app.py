1070a5592cb30af0f4a8020b75b97e4cf767db60
Wahid Contractor <wcontractor@gmail.com>
2024-03-26 19:53:17 -0400

    Update streamlit_app.py
    Updated fruity vice api and dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,6 @@ ingredients_list = st.multiselect(
     , max_selections=5
 )
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-
 if ingredients_list:
     st.write(ingredients_list)
     st.text(ingredients_list)
@@ -39,6 +36,9 @@ if ingredients_list:
 
     st.write(ingredients_string)
 
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+    
     #Create an insert statement to insert the selected fruits to the orders table
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
         values ('""" + ingredients_string + """', '"""+name_on_order+"""')"""
